74f900a42140f71f42db82947c57d75e7858f864
Compart, Torsten <torsten.compart@gmail.com>
2015-06-12 01:10:33 +0200

    introducing ui.router to project

--- nicoco-frontend/src/app/module.js
+++ nicoco-frontend/src/app/module.js
@@ -1,1 +1,13 @@
-angular.module('nicoco', []).value('greeting', 'Hello');
+angular.module('nicoco', ['ui.router']).config(function ($stateProvider, $urlRouterProvider) {
+
+	$urlRouterProvider.otherwise("/home");
+
+	$stateProvider.state('home', {
+		url: '/home',
+		template: '<header role="banner">' +
+		'<img id="logo-main" src="images/logo.jpg" />' +
+		'<carousel></carousel>' +
+		'<navigation></navigation>' +
+		'</header>'
+	});
+});
